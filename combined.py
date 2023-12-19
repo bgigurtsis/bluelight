02c48400b4bd7d2cdc43bc08af53e623d5ca0be7
Billy Gigurtsis <69985092+bgigurtsis@users.noreply.github.com>
2023-12-19 18:58:00 +0000

    Update combined.py

--- combined.py
+++ combined.py
@@ -1,52 +1,28 @@
-#!/usr/bin/env python3
-import bluetooth
-import bluetooth._bluetooth as bluez
-import time
-import sys
+import subprocess
+import re
 
-def get_connected_devices(sock):
-    """Get a list of connected devices."""
-    connected_devices = []
-    max_rsp = 255
-    mode = 0
-    results, num_rsp = bluez.hci_inquiry(sock, length=8, max_rsp=max_rsp, flags=mode)
+def get_connected_devices():
+    # Use 'bluetoothctl' to list connected devices
+    result = subprocess.run(['bluetoothctl', 'info'], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+    devices = re.findall(r'Device ([0-9A-F:]+) ', output)
+    return devices
 
-    for i in range(num_rsp):
-        addr = results[i][0]
-        name = bluetooth.lookup_name(addr)
-        connected_devices.append((addr, name))
-    
-    return connected_devices
-
-def get_rssi(sock, addr):
-    """Get the RSSI of a specific connected device."""
-    try:
-        rssi = bluez.hci_read_rssi(sock, addr)
-    except Exception as e:
-        print("Could not read RSSI for", addr)
-        print(e)
-        return None
-    return rssi
+def get_rssi(device_address):
+    # Use 'hcitool' to get the RSSI of a given device
+    result = subprocess.run(['hcitool', 'rssi', device_address], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+    rssi = re.search(r'RSSI return value: (-\d+)', output)
+    return int(rssi.group(1)) if rssi else None
 
 def main():
-    dev_id = 0
-    try:
-        sock = bluez.hci_open_dev(dev_id)
-        print("Bluetooth device opened successfully.")
-    except Exception as e:
-        print("Error accessing bluetooth device.")
-        print(e)
-        sys.exit(1)
-
-    connected_devices = get_connected_devices(sock)
-    if not connected_devices:
+    devices = get_connected_devices()
+    if devices:
+        for device in devices:
+            rssi = get_rssi(device)
+            print(f"Device {device} RSSI: {rssi}")
+    else:
         print("No connected Bluetooth devices found.")
-        return
-
-    for addr, name in connected_devices:
-        rssi = get_rssi(sock, addr)
-        if rssi is not None:
-            print(f"Device: {name} ({addr}), RSSI: {rssi}")
 
 if __name__ == "__main__":
     main()
