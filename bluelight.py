ba92cf92f8715173957060d8d223169d37f0e695
Billy Gigurtsis <69985092+bgigurtsis@users.noreply.github.com>
2023-12-19 15:38:46 +0000

    Update bluelight.py

--- bluelight.py
+++ bluelight.py
@@ -1,47 +1,36 @@
-import bluetooth
-import bluetooth._bluetooth as bluez
-import struct
-import sys
+import subprocess
+import re
 
-def device_inquiry_with_rssi(sock):
-    # Set inquiry parameters
-    duration = 8  # ~10.24 seconds; 1.28 * 8
-    max_responses = 255
-    cmd_pkt = struct.pack("BBBBB", 0x33, 0x8b, 0x9e, duration, max_responses)
-    bluez.hci_send_cmd(sock, bluez.OGF_LINK_CTL, bluez.OCF_INQUIRY, cmd_pkt)
-
-    # Process inquiry results
-    results = []
-    done = False
-    while not done:
-        pkt = sock.recv(255)
-        ptype, event, plen = struct.unpack("BBB", pkt[:3])
-        if event == bluez.EVT_INQUIRY_RESULT_WITH_RSSI:
-            pkt = pkt[3:]
-            nrsp = bluetooth.get_byte(pkt[0])
-            for i in range(nrsp):
-                addr = bluez.ba2str(pkt[1+6*i:1+6*i+6])
-                rssi = bluetooth.byte_to_signed_int(bluetooth.get_byte(pkt[1 + 13 * nrsp + i]))
-                results.append((addr, rssi))
-                print(f"Discovered: {addr}, RSSI: {rssi}")
-        elif event == bluez.EVT_INQUIRY_COMPLETE:
-            done = True
-
-    return results
-
-def main():
-    # Open Bluetooth device
-    dev_id = 0
+def get_connected_devices():
     try:
-        sock = bluez.hci_open_dev(dev_id)
-    except:
-        print("Error accessing Bluetooth device.")
-        sys.exit(1)
+        # Running 'hcitool con' to get a list of connected devices
+        hcitool_output = subprocess.check_output(['hcitool', 'con']).decode('utf-8')
+        # Extracting device addresses using regular expression
+        return re.findall(r'(?:>?\s)([0-9A-F:]{17})', hcitool_output)
+    except subprocess.CalledProcessError:
+        return []
 
-    # Start device inquiry
-    print("Scanning for devices...")
-    device_inquiry_with_rssi(sock)
-    print("Scan complete.")
+def get_device_rssi(device_address):
+    try:
+        # Running 'btmgmt find' to get device information including RSSI
+        btmgmt_output = subprocess.check_output(['sudo', 'btmgmt', 'find']).decode('utf-8')
+        # Regular expression to find the RSSI for the specific device
+        match = re.search(rf'{device_address}\s+.*\s+RSSI: (-?\d+)', btmgmt_output)
+        if match:
+            return int(match.group(1))
+        return None
+    except subprocess.CalledProcessError:
+        return None
 
-if __name__ == "__main__":
-    main()
+# Get list of connected Bluetooth devices
+connected_devices = get_connected_devices()
+print("Connected Bluetooth devices:")
+for device in connected_devices:
+    print(f"  Address: {device}")
+
+    # Get RSSI for each connected device
+    rssi = get_device_rssi(device)
+    if rssi is not None:
+        print(f"  RSSI: {rssi}")
+    else:
+        print("  RSSI: Not available")
