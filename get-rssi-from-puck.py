3983dfcc56cbee0fbe224b56c04dfcade06b02b9
Billy Gigurtsis <69985092+bgigurtsis@users.noreply.github.com>
2023-12-20 15:58:01 +0000

    Update get-rssi-from-puck.py

--- get-rssi-from-puck.py
+++ get-rssi-from-puck.py
@@ -6,21 +6,16 @@ async def run():
     target_device_address = "C9:FA:4B:21:11:26"
 
     while True:
-        devices = await BleakScanner.discover()
-        found = False
-
-        for device in devices:
+        discovered = await BleakScanner.discover(timeout=5.0, return_adv=True)
+        for address, (device, advertisement_data) in discovered.items():
             if device.name == target_device_name or device.address == target_device_address:
-                advertisement_data = device.metadata['advertisement_data']
                 rssi = advertisement_data.rssi
                 print(f"Found {target_device_name}: Address={device.address}, RSSI={rssi}")
-                found = True
                 break
-
-        if not found:
+        else:
             print(f"{target_device_name} not found.")
 
-        await asyncio.sleep(5)  # Delay in seconds before rescanning
+        await asyncio.sleep(1)  # Delay before rescanning
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
