ce607832707484174646c0b5457fc8a680636f85
Billy Gigurtsis <69985092+bgigurtsis@users.noreply.github.com>
2023-12-20 15:24:37 +0000

    Update get-rssi-from-puck.py

--- get-rssi-from-puck.py
+++ get-rssi-from-puck.py
@@ -1,25 +1,22 @@
-from bluepy import btle
+from bluepy.btle import Scanner
 
-def get_rssi():
-    device_address = "C9:FA:4B:21:11:26"  # Hardcoded MAC address of the Puck.js
-    try:
-        # Connect to the device
-        peripheral = btle.Peripheral(device_address, "random")
-        # Get RSSI
-        rssi = peripheral.rssi
-        # Disconnect
-        peripheral.disconnect()
-        return rssi
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
+def get_rssi_from_scan(device_address):
+    scanner = Scanner()
+    devices = scanner.scan(10.0)  # Scanning for 10 seconds
+
+    for dev in devices:
+        if dev.addr == device_address:
+            return dev.rssi
+    return None
 
 def main():
-    rssi = get_rssi()
+    device_address = "C9:FA:4B:21:11:26"  # Hardcoded MAC address of the Puck.js
+    rssi = get_rssi_from_scan(device_address)
+
     if rssi is not None:
         print(f"Device {device_address} RSSI: {rssi}")
     else:
-        print("Failed to retrieve RSSI.")
+        print("Puck.js not found.")
 
 if __name__ == "__main__":
     main()
