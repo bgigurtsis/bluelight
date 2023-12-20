87a0e15aa16350627412f088b811b9b3cb09969c
Billy Gigurtsis <69985092+bgigurtsis@users.noreply.github.com>
2023-12-20 15:21:37 +0000

    Update bluelight.py

--- bluelight.py
+++ bluelight.py
@@ -1,36 +1,82 @@
 import subprocess
 import re
+import threading
+import time
+import random
+from gpiozero import PWMOutputDevice
+from guizero import App, Slider
+
+# Set up the GPIO for the LED
+pin = PWMOutputDevice(18)
+
+# Default intensity when beacon is not detected (range 0 to 1)
+default_intensity = 0.1  # Lower default intensity to symbolize calmness
 
 def get_connected_devices():
-    try:
-        # Running 'hcitool con' to get a list of connected devices
-        hcitool_output = subprocess.check_output(['hcitool', 'con']).decode('utf-8')
-        # Extracting device addresses using regular expression
-        return re.findall(r'(?:>?\s)([0-9A-F:]{17})', hcitool_output)
-    except subprocess.CalledProcessError:
-        return []
+    # Use 'bluetoothctl' to list connected devices
+    result = subprocess.run(['bluetoothctl', 'info'], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+    devices = re.findall(r'Device ([0-9A-F:]+) ', output)
+    return devices
 
-def get_device_rssi(device_address):
-    try:
-        # Running 'btmgmt find' to get device information including RSSI
-        btmgmt_output = subprocess.check_output(['sudo', 'btmgmt', 'find']).decode('utf-8')
-        # Regular expression to find the RSSI for the specific device
-        match = re.search(rf'{device_address}\s+.*\s+RSSI: (-?\d+)', btmgmt_output)
-        if match:
-            return int(match.group(1))
-        return None
-    except subprocess.CalledProcessError:
-        return None
+def get_rssi(device_address):
+    # Use 'hcitool' to get the RSSI of a given device
+    result = subprocess.run(['hcitool', 'rssi', device_address], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+    rssi = re.search(r'RSSI return value: (-?\d+)', output)
+    return int(rssi.group(1)) if rssi else None
 
-# Get list of connected Bluetooth devices
-connected_devices = get_connected_devices()
-print("Connected Bluetooth devices:")
-for device in connected_devices:
-    print(f"  Address: {device}")
+def map_value(x, in_min, in_max, out_min, out_max):
+    """Map a value from one range to another."""
+    # Adjust RSSI value to handle positive values and 0
+    x = min(x, 0)
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-    # Get RSSI for each connected device
-    rssi = get_device_rssi(device)
-    if rssi is not None:
-        print(f"  RSSI: {rssi}")
+def flicker_intensity(intensity):
+    """Apply flickering effect to the LED based on intensity."""
+    if intensity > 0:
+        on_time = random.uniform(0.05, max(0.1, 1 - intensity))
+        off_time = random.uniform(0.05, max(0.1, 1 - intensity))
+        pin.on()
+        time.sleep(on_time)
+        pin.off()
+        time.sleep(off_time)
     else:
-        print("  RSSI: Not available")
+        pin.off()
+
+def auto_control():
+    """Automatically control LED based on Bluetooth beacon's RSSI to represent emotional intensity."""
+    while True:
+        devices = get_connected_devices()
+        intensity = default_intensity
+        if devices:
+            for device in devices:
+                rssi = get_rssi(device)
+                if rssi is not None:
+                    # Intensity increases as RSSI gets stronger (closer)
+                    intensity = map_value(rssi, -100, 0, 1, 0)
+                    print(f"Device {device} RSSI: {rssi}. Emotional Intensity: {intensity}")
+                else:
+                    print(f"Device {device} RSSI not available. Default calm state.")
+
+        intensity = max(0, min(1, intensity))  # Constrain to 0-1
+        flicker_intensity(intensity)
+        time.sleep(0.1)  # Delay between flickers
+
+def slider_changed(percent):
+    """Manually control the LED intensity using the slider."""
+    pin.value = int(percent) / 100
+
+def main():
+    # Start the auto control thread
+    thread = threading.Thread(target=auto_control, daemon=True)
+    thread.start()
+
+    # Set up the GUI
+    app = App(title='Emotional Intensity Control', width=500, height=150)
+    slider = Slider(app, command=slider_changed, width='fill', height=50)
+    slider.text_size = 30
+    app.display()
+
+if __name__ == "__main__":
+    main()
