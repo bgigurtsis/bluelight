5b7cd10329a3bfabd07aea708b1807cf683b3899
Billy Gigurtsis <69985092+bgigurtsis@users.noreply.github.com>
2023-12-22 13:56:44 +0000

    Update bluelight.py

--- bluelight.py
+++ bluelight.py
@@ -1,82 +1,64 @@
-import subprocess
-import re
+import asyncio
 import threading
-import time
-import random
 from gpiozero import PWMOutputDevice
-from guizero import App, Slider
+from bleak import BleakScanner
 
 # Set up the GPIO for the LED
 pin = PWMOutputDevice(18)
 
 # Default intensity when beacon is not detected (range 0 to 1)
-default_intensity = 0.1  # Lower default intensity to symbolize calmness
+default_intensity = 0.0  # Off when not detected
 
-def get_connected_devices():
-    # Use 'bluetoothctl' to list connected devices
-    result = subprocess.run(['bluetoothctl', 'info'], stdout=subprocess.PIPE)
-    output = result.stdout.decode('utf-8')
-    devices = re.findall(r'Device ([0-9A-F:]+) ', output)
-    return devices
-
-def get_rssi(device_address):
-    # Use 'hcitool' to get the RSSI of a given device
-    result = subprocess.run(['hcitool', 'rssi', device_address], stdout=subprocess.PIPE)
-    output = result.stdout.decode('utf-8')
-    rssi = re.search(r'RSSI return value: (-?\d+)', output)
-    return int(rssi.group(1)) if rssi else None
+# Global variable to hold the latest RSSI value
+latest_rssi = None
 
 def map_value(x, in_min, in_max, out_min, out_max):
     """Map a value from one range to another."""
-    # Adjust RSSI value to handle positive values and 0
-    x = min(x, 0)
+    x = max(min(x, in_max), in_min)
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-def flicker_intensity(intensity):
-    """Apply flickering effect to the LED based on intensity."""
-    if intensity > 0:
-        on_time = random.uniform(0.05, max(0.1, 1 - intensity))
-        off_time = random.uniform(0.05, max(0.1, 1 - intensity))
-        pin.on()
-        time.sleep(on_time)
-        pin.off()
-        time.sleep(off_time)
-    else:
-        pin.off()
+def adjust_led_intensity(intensity):
+    """Adjust LED intensity based on the mapped value."""
+    pin.value = intensity
 
 def auto_control():
-    """Automatically control LED based on Bluetooth beacon's RSSI to represent emotional intensity."""
+    """Automatically control LED based on Bluetooth beacon's RSSI to represent signal strength."""
+    global latest_rssi
     while True:
-        devices = get_connected_devices()
         intensity = default_intensity
-        if devices:
-            for device in devices:
-                rssi = get_rssi(device)
-                if rssi is not None:
-                    # Intensity increases as RSSI gets stronger (closer)
-                    intensity = map_value(rssi, -100, 0, 1, 0)
-                    print(f"Device {device} RSSI: {rssi}. Emotional Intensity: {intensity}")
-                else:
-                    print(f"Device {device} RSSI not available. Default calm state.")
+        if latest_rssi is not None:
+            # Intensity increases as RSSI gets stronger (closer)
+            intensity = map_value(latest_rssi, -85, -40, 0, 1)
+            print(f"RSSI: {latest_rssi}, Mapped Intensity: {intensity:.2f}")
 
         intensity = max(0, min(1, intensity))  # Constrain to 0-1
-        flicker_intensity(intensity)
-        time.sleep(0.1)  # Delay between flickers
+        adjust_led_intensity(intensity)
 
-def slider_changed(percent):
-    """Manually control the LED intensity using the slider."""
-    pin.value = int(percent) / 100
+async def rssi_scanning():
+    """Asynchronous method to continuously scan for BLE devices and update RSSI."""
+    global latest_rssi
+    target_device_address = "C9:FA:4B:21:11:26"  # Replace with your device's MAC address
+
+    while True:
+        devices = await BleakScanner.discover(timeout=2.0)
+        for device in devices:
+            if device.address == target_device_address:
+                latest_rssi = device.rssi
+                break
+        await asyncio.sleep(0.1)
+
+def start_async_loop(loop):
+    """Starts the asynchronous event loop."""
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(rssi_scanning())
 
 def main():
-    # Start the auto control thread
-    thread = threading.Thread(target=auto_control, daemon=True)
-    thread.start()
+    # Start the BLE RSSI scanning in a separate thread
+    loop = asyncio.new_event_loop()
+    threading.Thread(target=start_async_loop, args=(loop,), daemon=True).start()
 
-    # Set up the GUI
-    app = App(title='Emotional Intensity Control', width=500, height=150)
-    slider = Slider(app, command=slider_changed, width='fill', height=50)
-    slider.text_size = 30
-    app.display()
+    # Start the auto control thread
+    threading.Thread(target=auto_control, daemon=True).start()
 
 if __name__ == "__main__":
     main()
