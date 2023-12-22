79f0b88c30ec3eb1ecc52865455765ec83560fec
Billy Gigurtsis <69985092+bgigurtsis@users.noreply.github.com>
2023-12-22 14:58:33 +0000

    Update bluelight.py

--- bluelight.py
+++ bluelight.py
@@ -7,39 +7,22 @@ from bleak import BleakScanner
 pin = PWMOutputDevice(18)
 
 # Default intensity when beacon is not detected (range 0 to 1)
-default_intensity = 0.0  # Off when not detected
+default_intensity = 0.0
 
 # Global variable to hold the latest RSSI value
 latest_rssi = None
-
-def map_value(x, in_min, in_max, out_min, out_max):
-    """Map a value from one range to another."""
-    x = max(min(x, in_max), in_min)
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+should_continue = True
 
 def adjust_led_intensity(intensity):
     """Adjust LED intensity based on the mapped value."""
     pin.value = intensity
 
-def auto_control():
-    """Automatically control LED based on Bluetooth beacon's RSSI to represent signal strength."""
-    global latest_rssi
-    while True:
-        intensity = default_intensity
-        if latest_rssi is not None:
-            # Intensity increases as RSSI gets stronger (closer)
-            intensity = map_value(latest_rssi, -85, -40, 0, 1)
-            print(f"RSSI: {latest_rssi}, Mapped Intensity: {intensity:.2f}")
-
-        intensity = max(0, min(1, intensity))  # Constrain to 0-1
-        adjust_led_intensity(intensity)
-
 async def rssi_scanning():
     """Asynchronous method to continuously scan for BLE devices and update RSSI."""
-    global latest_rssi
-    target_device_address = "C9:FA:4B:21:11:26"  # Replace with your device's MAC address
+    global latest_rssi, should_continue
+    target_device_address = "C9:FA:4B:21:11:26"
 
-    while True:
+    while should_continue:
         devices = await BleakScanner.discover(timeout=2.0)
         for device in devices:
             if device.address == target_device_address:
@@ -47,18 +30,35 @@ async def rssi_scanning():
                 break
         await asyncio.sleep(0.1)
 
-def start_async_loop(loop):
-    """Starts the asynchronous event loop."""
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(rssi_scanning())
+def auto_control():
+    """Automatically control LED based on Bluetooth beacon's RSSI."""
+    global latest_rssi, should_continue
+    while should_continue:
+        intensity = default_intensity
+        if latest_rssi is not None:
+            # Modify this mapping as needed
+            intensity = (latest_rssi + 85) / (-40 + 85)
+            print(f"RSSI: {latest_rssi}, Intensity: {intensity:.2f}")
+
+        adjust_led_intensity(max(0, min(1, intensity)))
+        time.sleep(0.1)
 
 def main():
-    # Start the BLE RSSI scanning in a separate thread
     loop = asyncio.new_event_loop()
-    threading.Thread(target=start_async_loop, args=(loop,), daemon=True).start()
+    scan_thread = threading.Thread(target=start_async_loop, args=(loop,))
+    control_thread = threading.Thread(target=auto_control)
 
-    # Start the auto control thread
-    threading.Thread(target=auto_control, daemon=True).start()
+    scan_thread.start()
+    control_thread.start()
+
+    try:
+        scan_thread.join()
+        control_thread.join()
+    except KeyboardInterrupt:
+        global should_continue
+        should_continue = False
+        scan_thread.join()
+        control_thread.join()
 
 if __name__ == "__main__":
     main()
